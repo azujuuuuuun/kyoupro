dd63c01ac88ddba63b9ae4ce8667cfca64a3af2f
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-02-07 19:59:11 +0900

    [add]ABC085（Cまで）の追加

---
+++ AtCoder/ABC/085/b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main (void) {
+    int n;
+    set<int> s;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int d;
+        cin >> d;
+        s.insert(d);
+    }
+
+    cout << s.size() << endl;
+    
+    return 0;
+}
