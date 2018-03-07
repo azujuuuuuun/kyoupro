adf3e175b40d90cf08938e707f07c2c2acbe531f
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-07 23:24:52 +0900

    [add]ABC060（A）を追加

---
+++ AtCoder/ABC/060/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    string A, B, C;
+
+    cin >> A >> B >> C;
+
+    if (A[A.length() - 1] == B[0] && B[B.length() - 1] == C[0]) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    
+    return 0;
+}
