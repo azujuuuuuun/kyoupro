1e0973757d8a4d4d4460ac1d90ddcfa3acac11ab
azujuuuuuun <azujuuuuuun@gmail.com>
2018-03-11 17:06:42 +0900

    [add]ABC047（A）を追加

---
+++ AtCoder/ABC/047/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (a + b == c || b + c == a || c + a == b) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
