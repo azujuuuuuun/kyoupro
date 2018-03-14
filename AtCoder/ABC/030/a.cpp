9f7c765b2f3f3f6c5399039d4388f2b1f67ec49e
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-14 21:34:51 +0900

    [add]ABC030（A）を追加

---
+++ AtCoder/ABC/030/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    double A, B, C, D;
+
+    cin >> A >> B >> C >> D;
+
+    if (B / A > D / C) cout << "TAKAHASHI" << endl;
+    else if (B / A < D / C) cout << "AOKI" << endl;
+    else cout << "DRAW" << endl;
+
+    return 0;
+}
