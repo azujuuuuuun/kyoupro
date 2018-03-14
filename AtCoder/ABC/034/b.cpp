094a2cd563485d49867d85359c8ced7e2e9a494c
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-14 20:01:44 +0900

    [add]ABC034（Bまで）を追加

---
+++ AtCoder/ABC/034/b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int n;
+
+    cin >> n;
+
+    if (n % 2 == 0) cout << n - 1 << endl;
+    else cout << n + 1 << endl;
+    
+    return 0;
+}
