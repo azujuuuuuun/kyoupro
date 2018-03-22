997d2cb356c82033e11a932a0d498fca54b6b2b8
azujuuuuuun <azujuuuuuun@gmail.com>
2018-03-22 17:18:12 +0900

    [add]ABC091（Bまで）を追加

---
+++ AtCoder/ABC/091/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int A;
+    int B;
+    int C;
+
+    cin >> A >> B >> C;
+
+    if (A + B >= C) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
