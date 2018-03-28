a415f59e9bce3f9a7b0cf30eb6e536d87dff5565
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-28 18:33:51 +0900

    [add]ABC020（Bまで）を追加

---
+++ AtCoder/ABC/020/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int Q;
+
+    cin >> Q;
+
+    if (Q == 1) {
+        cout << "ABC" << endl;
+    } else {
+        cout << "chokudai" << endl;
+    }
+
+    return 0;
+}
---
+++ AtCoder/ABC/020/b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    string A, B;
+
+    cin >> A >> B;
+
+    cout << 2 * stoi(A + B) << endl;
+
+    return 0;
+}
