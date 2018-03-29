8bd0d40bca88cfd70e26363a2c93d80eba91ac53
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-29 19:34:10 +0900

    [add]ABC010（Cまで）を追加

---
+++ AtCoder/ABC/010/b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int n;
+
+    cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        while (a % 2 == 0 || a % 3 == 2) {
+            a--;
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
