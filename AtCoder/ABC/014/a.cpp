be95605b7da2c662ee51c96401a0a6944c8f87fc
azujuuuuuun <azujuuuuuun@gmail.com>
2018-03-29 18:10:50 +0900

    [add]ABC014（A）を追加

---
+++ AtCoder/ABC/014/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int a;
+    int b;
+
+    cin >> a;
+    cin >> b;
+
+    if (a % b == 0){
+        cout << 0 << endl;
+    } else {
+        cout << b - (a % b) << endl;
+    }
+
+    return 0;
+}
