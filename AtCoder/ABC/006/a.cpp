3f14967dc78a3bf30e6ac68a5d4d92740635a021
azujuuuuuun <azujuuuuuun@gmail.com>
2018-03-31 00:14:27 +0900

    [add]ABC006（Bまで）を追加

---
+++ AtCoder/ABC/006/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int N;
+
+    cin >> N;
+
+    if (N % 3 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
