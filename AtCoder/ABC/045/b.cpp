b3c264e3356e7f6e63ec3645a78fa11c560fbc20
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-04-02 12:23:53 +0900

    [add]ABC045（B）を追加

--- AtCoder/ABC/045/b.cpp
+++ AtCoder/ABC/045/b.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 using namespace std;
+const int N = 3;
 int main (void) {
-    string SA, SB , SC;
+    string S[N];
 
-    cin >> SA >> SB >> SC;
+    for (int i = 0; i < N; i++) {
+        cin >> S[i];
+    }
+
+    int turn = 0;
+    int pos[N] = {0};
+    int len[N];
+    for (int i = 0; i < N; i++) {
+        len[i] = S[i].length();
+    }
+    while (true) {
+        if (pos[turn] == len[turn]) {
+            break;
+        }
+        char c = S[turn][pos[turn]];
+        pos[turn]++;
+        if (c == 'a') {
+            turn = 0;
+        } else if (c == 'b') {
+            turn = 1;
+        } else {
+            turn = 2;
+        }
+    }
+
+    if (turn == 0) {
+        cout << 'A' << endl;
+    } else if (turn == 1) {
+        cout << 'B' << endl;
+    } else {
+        cout << 'C' << endl;
+    }
+
     return 0;
 }
