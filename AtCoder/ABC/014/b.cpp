501b219d4d94aee30838783fccc170efe98aad60
azujuuuuuun <azujuuuuuun@gmail.com>
2018-04-02 11:55:09 +0900

    [add]ABC014（B）を追加

---
+++ AtCoder/ABC/014/b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int n;
+    int X;
+
+    cin >> n >> X;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if ((X >> i) & 1) {
+            ans += a;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
