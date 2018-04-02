730eaea5d41079d5345ead3e5d51e02e36296b5e
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-04-02 13:50:20 +0900

    [add]ABC054（B）を追加

--- AtCoder/ABC/054/b.cpp
+++ AtCoder/ABC/054/b.cpp
@@ -11,6 +11,25 @@ int main (void) {
     for (int i = 0; i < N; i++) cin >> A[i];
     for (int i = 0; i < M; i++) cin >> B[i];
 
-    
+    int H = N - M;
+    int BW = B[0].length();
+    int W = A[0].length() - BW;
+    for (int i = 0; i <= H; i++) {
+        for (int j = 0; j <= W; j++) {
+            bool match = true;
+            for (int k = 0; k < M; k++) {
+                if (A[i + k].substr(j, BW) != B[k]) {
+                    match = false;
+                }
+            }
+            if (match) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No" << endl;
+
     return 0;
 }
