26bdd1a5a96edd2789f633f661e039c6806912de
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-04-04 15:12:34 +0900

    [add]ARC016（A）を追加

---
+++ AtCoder/ARC/016/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int N;
+    int M;
+
+    cin >> N >> M;
+
+    if (M == 1) {
+        cout << N << endl;
+    } else {
+        cout << 1 << endl;
+    }
+    
+    return 0;
+}
