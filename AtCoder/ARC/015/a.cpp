cbb09a0046541ff09ab5e471f5d8e665cef5e0bc
azujuuuuuun <azujuuuuuun@gmail.com>
2018-04-04 15:09:23 +0900

    [add]ARC015（A）を追加

---
+++ AtCoder/ARC/015/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+    int n;
+
+    cin >> n;
+
+    double f = ((double)n * 9 / 5) + 32;
+
+    cout << f << endl;
+    
+    return 0;
+}
