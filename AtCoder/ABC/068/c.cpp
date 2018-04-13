22cc0193804d84e3c957a8a396a26994237d93c2
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-04-13 12:40:35 +0900

    [add]ABC068（C）を追加

--- AtCoder/ABC/068/c.cpp
+++ AtCoder/ABC/068/c.cpp
@@ -1,47 +1,29 @@
 #include <iostream>
 using namespace std;
-const int NM_MAX = 200000;
-int N;
-int M;
-bool c[NM_MAX][NM_MAX];
-bool dfs (int v, int cnt, bool visited[NM_MAX]) {
-    visited[v] = true;
-    if (cnt == 2) {
-        if (v == N) return true;
-        else return false;
-    } else {
-        for (int i = 0; i < N; i++) {
-            if (c[v][i] && !visited[i]) return dfs(i, cnt + 1, visited);
-        }
-    }
-    return false;
-}
 int main (void) {
+    int N;
+    int M;
+
     cin >> N >> M;
-
-    N--;
-
+    int from1[N] = {0};
+    int toN[N] = {0};
     for (int i = 0; i < M; i++) {
-        for (int j = 0; j < M; j++) {
-            c[i][j] = false;
-        }
-    }
-
-    for (int i = 0; i < M; i++) {
-        int a;
-        int b;
+        int a, b;
         cin >> a >> b;
         a--;
         b--;
-        c[a][b] = true;
-        c[b][a] = true;
+        if (a == 0) from1[b] = 1;
+        if (b == N - 1) toN[a] = 1;
+    }
+    
+    for (int i = 0; i < N; i++) {
+        if (from1[i] && toN[i]) {
+            cout << "POSSIBLE" << endl;
+            return 0;
+        }
     }
 
-    bool visited[N];
-    for (int i = 0; i < N; i++) visited[i] = false;
-
-    if (dfs(0, 0, visited)) cout << "POSSIBLE" << endl;
-    else cout << "IMPOSSIBLE" << endl;
+    cout << "IMPOSSIBLE" << endl;
 
     return 0;
 }
