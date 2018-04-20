bb4d52af0c06dec5781249317169d1843dd506a9
azujuuuuuun <azujuuuuuun@gmail.com>
2018-04-20 10:58:14 +0900

    [add]ABC091（C）を追加

--- AtCoder/ABC/091/c.cpp
+++ AtCoder/ABC/091/c.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int main (void) {
+    int N;
+
+    cin >> N;
+    vector<pair<int, int> > r;
+    vector<pair<int, int> > b;
+    for (int i = 0; i < N; i++) {
+        int ai, bi;
+        cin >> ai >> bi;
+        r.push_back(make_pair(-bi, ai));
+    }
+    for (int i = 0; i < N; i++) {
+        int ci, di;
+        cin >> ci >> di;
+        b.push_back(make_pair(ci, di));
+    }
+
+    sort(r.begin(), r.end());
+    sort(b.begin(), b.end());
+
+    for (int i = 0; i < N; i++) {
+        int tmp = -r[i].first;
+        r[i].first = r[i].second;
+        r[i].second = tmp;
+    }
+
+    bool rp[N];
+    bool bp[N];
+    for (int i = 0; i < N; i++) {
+        rp[i] = false;
+        bp[i] = false;
+    }
+    int ans = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (rp[j]) {
+                continue;
+            }
+            if (bp[i]) {
+                break;
+            }
+            if (b[i].first > r[j].first && b[i].second > r[j].second) {
+                rp[j] = true;
+                bp[i] = true;
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
