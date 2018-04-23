47d5d831a48fce88f779e8e069f2b6315d0fd837
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-04-23 17:19:59 +0900

    [add]ABC043（D）を追加

--- AtCoder/ABC/043/d.cpp
+++ AtCoder/ABC/043/d.cpp
@@ -4,13 +4,18 @@ int main (void) {
     string s;
 
     cin >> s;
-    int cnt[26] = {0};
-    for (int i = 0; i < s.length(); i++) cnt[s[i] - 'a']++;
 
-    int ma = 0;
-    for (int i = 0; i < 26; i++) ma = max(ma, cnt[i]);
+    for (int i = 0, len = s.length(); i < len - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            cout << i + 1 << " " << i + 2 << endl;
+            return 0;
+        } else if (i + 2 <= len - 1 && s[i] == s[i + 2]) {
+            cout << i + 1 << " " << i + 3 << endl;
+            return 0;
+        }
+    }
 
-    if (ma == 1) cout << -1 << " " << -1 << endl;
-    
+    cout << "-1 -1" << endl;
+
     return 0;
 }
