6b40c617c54b76f16f2c0ce4b858137cb82f95fa
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-06-16 15:47:37 +0900

    [add]ABC099（Aのみ）を追加

---
+++ AtCoder/ABC/099/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+  int N;
+
+  cin >> N;
+
+  if (N <= 999) {
+    cout << "ABC" << endl;
+  } else {
+    cout << "ABD" << endl;
+  }
+
+  return 0;
+}
